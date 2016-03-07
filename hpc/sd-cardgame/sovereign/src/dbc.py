4cbb8f702842ce49b4163eb1aaf64fbb6be54e11
Alexey Tarutin <alex.tarutin@gmail.com>
2016-03-07 19:50:52 +0000

    Added json for cards

---
+++ hpc/sd-cardgame/sovereign/src/dbc.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+
+import sys, os
+import itertools
+import json
+import argparse
+
+
+class Sovereign(object):
+    def __init__(self):
+        self.play
+
+
+    def load_cards():
+        with open(jsonfile) as card_file:
+            cards = json.load(card_file)
+            
+        return cards
+
+
